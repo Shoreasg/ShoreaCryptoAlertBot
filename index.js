a2c7e5a742d1e2b0046526a5672a578d7ca94f6d
Wei Lun <garyganweilun@gmail.com>
2021-05-16 22:52:16 +0800

    test

--- index.js
+++ index.js
@@ -8,16 +8,30 @@ const binanceClient = Binance.default({
     apiKey: process.env.BINANCE_API_KEY,
     apiSecret: process.env.BINANCE_API_SECRETKEY,
 })
+const Telebot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true }); //from github.com/yagop/node-telegram-bot-api 
+
+Telebot.onText(/\/Type of Coin: (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
 
 
-const CryptoCoin1 = 'ADA'
-const CryptoCoin2 = 'USDT'
+    // tell user received message, retriving data.
+    bot.sendMessage(chatId, "Retriving data");
 
-binanceClient
-    .avgPrice({ symbol: `${CryptoCoin1}${CryptoCoin2}` }) // example, { symbol: "BTCUSTD" }
-    .then((avgPrice) => {
-        console.log(formatMoney(avgPrice['price']))
-    })
-    .catch((error) =>
-        console.log(`Error retrieving the price for ${cryptoToken1}${cryptoToken2}: ${error}`)
-    )
+    const CryptoCoin1 = 'ADA'
+    const CryptoCoin2 = 'USDT'
+    binanceClient
+        .avgPrice({ symbol: `${CryptoCoin1}${CryptoCoin2}` }) // example, { symbol: "BTCUSTD" }
+        .then((avgPrice) => {
+            console.log(formatMoney(avgPrice['price']))
+        })
+        .catch((error) =>
+            console.log(`Error retrieving the price for ${cryptoToken1}${cryptoToken2}: ${error}`)
+        )
+});
+
+
+
