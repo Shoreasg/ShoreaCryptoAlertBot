afea3ca20f4034546c9cfb06544097af14d33d74
Wei Lun <garyganweilun@gmail.com>
2021-05-17 22:14:02 +0800

    changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.onText(/\/coin (.+)/, (msg, match) => {
     binanceClient
         .avgPrice({ symbol: `${CryptoCoin1}${CryptoCoin2}`.toUpperCase() })
         .then((avgPrice) => {
-            bot.sendMessage(chatId, `<code>The Price for ${CryptoCoin1.toUpperCase()}${CryptoCoin2.toUpperCase()}: ${(avgPrice['price'])}</code>`);
+            bot.sendMessage(chatId, `<code>The Price for ${CryptoCoin1.toUpperCase()}${CryptoCoin2.toUpperCase()}: ${(avgPrice['price'])}</code>`, parse_mode: 'HTML'});
         })
         .catch((error) =>
             bot.sendMessage(chatId, `Error retrieving the price for ${CryptoCoin1}${CryptoCoin2}: ${error}`));
