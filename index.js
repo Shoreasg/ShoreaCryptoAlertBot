08b171ade331e00170e84ab906d583311fd14dd6
Wei Lun <garyganweilun@gmail.com>
2021-05-17 17:52:31 +0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Binance from 'binance-api-node'
 import TelegramBot from 'node-telegram-bot-api'
 import dotenv from 'dotenv'
-import {formatMoney } from './Utils/money.js'
+import { formatMoney } from './Utils/money.js'
 dotenv.config()
 
 
@@ -14,38 +14,35 @@ const binanceClient = Binance.default({
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true })
 //from github.com/yagop/node-telegram-bot-api
 
-bot.onText(/\/coin (.+)/, (msg1,data1) => {
+bot.onText(/\/coin (.+)/, (msg1, data1) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
 
     const chatId1 = msg1.chat.id;
-    const CryptoCoin1 = data1[1];
-
-
 
     // tell user received message, retriving data.
     bot.sendMessage(chatId1, "Please enter the pair that you would like to pair with.");
 
+    bot.on('message', (msg2) => {
+        const chatId2 = msg2.chat.id;
 
-   
+        bot.sendMessage(chatId2, "Retriving data.....");
 
-});
 
-bot.onText(/\/coin2 (.+)/, (msg2, data2) => {
-    const chatId2 = msg2.chat.id;
-    const CryptoCoin2 = data2[1];
-    bot.sendMessage(chatId2, "Retriving data.....");
+    });
 
+    const [CryptoCoin1 , CryptoCoin2 ="USDT"] = match[1].split(" ")
 
     binanceClient
         .avgPrice({ symbol: `${CryptoCoin1}${CryptoCoin2}` }) // example, { symbol: "BTCUSTD" }
         .then((avgPrice) => {
-            bot.sendMessage(chatId2, `The Price for ${CryptoCoin1}${CryptoCoin2}: ${formatMoney(avgPrice['price'])}`);
+            bot.sendMessage(chatId, `The Price for ${CryptoCoin1}${CryptoCoin2}: ${formatMoney(avgPrice['price'])}`);
         })
         .catch((error) =>
-            bot.sendMessage(chatId2, `Error retrieving the price for ${CryptoCoin1}${CryptoCoin2}: ${error}`));
-
+            bot.sendMessage(chatId, `Error retrieving the price for ${CryptoCoin1}${CryptoCoin2}: ${error}`));
+   
+        
 });
 
 bot.on('message', (msg) => {
