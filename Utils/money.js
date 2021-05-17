155768644c318bc834835f0a27e183ac802d9083
Wei Lun <garyganweilun@gmail.com>
2021-05-17 18:36:38 +0800

    changes

--- Utils/money.js
+++
@@ -1,3 +0,0 @@
-export const formatMoney = (value, currency = "USD", locale = "en-US") => {
-    return Intl.NumberFormat(locale, { style: "currency", currencyDisplay: "symbol", currency }).format(value);
-};
