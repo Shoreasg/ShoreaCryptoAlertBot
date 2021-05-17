0939b11efe0ed0572a21d3ff4af0534d3d9fe5da
Wei Lun <garyganweilun@gmail.com>
2021-05-17 15:22:05 +0800

    revert changes

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ import dotenv from 'dotenv'
 import { formatMoney } from './Utils/money.js'
 dotenv.config()
 
-import https from 'https'
-import express from 'express'
-import fs from 'fs'
-import 'env.ts'
 
-const app = express();
-
-
-const server = https.createServer({
-    key: fs.readFileSync('path/to/server.key'),
-    cert: fs.readFileSync('path/to/server.cert')
-}, app);
 
 const binanceClient = Binance.default({
     apiKey: process.env.BINANCE_API_KEY,
