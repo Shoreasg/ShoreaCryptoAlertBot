656fe668cdeec8a48b5b2488dd2db95d5bf3f1b7
Wei Lun <garyganweilun@gmail.com>
2021-05-17 22:25:52 +0800

    added parse_mode

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.onText(/\/coin (.+)/, (msg, match) => {
     binanceClient
         .avgPrice({ symbol: `${CryptoCoin1}${CryptoCoin2}`.toUpperCase() })
         .then((avgPrice) => {
-            bot.sendMessage(chatId, `The Price for ${CryptoCoin1.toUpperCase()}${CryptoCoin2.toUpperCase()}: $<code>{(avgPrice['price'])</code>}`, {parse_mode:"HTML"});
+            bot.sendMessage(chatId, `The Price for ${CryptoCoin1.toUpperCase()}${CryptoCoin2.toUpperCase()}: <code>${(avgPrice['price'])}</code>`, {parse_mode:"HTML"});
         })
         .catch((error) =>
             bot.sendMessage(chatId, `Error retrieving the price for ${CryptoCoin1}${CryptoCoin2}: ${error}`));
